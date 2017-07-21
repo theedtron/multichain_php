a82ab41c1456395ee453536d0348143aeb4c012f
theed <ekareo@wifiednetworks.co.ke>
2017-07-22 01:07:04 +0300

    debug

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\Bitcoin;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use be\kunstmaan\multichain\MultichainClient;
 use be\kunstmaan\multichain\MultichainHelper;
@@ -13,6 +14,7 @@ class TestController extends Controller
     public function testCmds(){
 
         $chained = new MultichainClient('http://'.env('JSON_RPC_URL').':'.env('JSON_RPC_PORT'), env('JSON_RPC_USERNAME'), env('JSON_RPC_PASSWORD'), 3);
+//        print_r($chained);
         $res = $chained->setDebug(true)->getInfo();
 
         print_r($res);
@@ -21,6 +23,9 @@ class TestController extends Controller
     public function easyCoin(){
 
         $bitcoin = new Bitcoin(env('JSON_RPC_USERNAME'),env('JSON_RPC_PASSWORD'),env('JSON_RPC_URL'),env('JSON_RPC_PORT'));
+        print_r($bitcoin);
+        print_r(' '.PHP_EOL);
+
         $res = $bitcoin->getinfo();
         print_r($bitcoin->error.PHP_EOL);
         print_r($bitcoin->response.PHP_EOL);
